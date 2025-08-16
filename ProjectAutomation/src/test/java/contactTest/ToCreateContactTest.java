c181ffc572f8b28f05b8ca0963141a23cb6bfd8e
gurra <gurra@gurramprasunsai>
2025-08-16 09:29:28 +0530

    sharing

--- ProjectAutomation/src/test/java/contactTest/ToCreateContactTest.java
+++ ProjectAutomation/src/test/java/contactTest/ToCreateContactTest.java
@@ -3,7 +3,7 @@ package contactTest;
 public class ToCreateContactTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("----vijetha-----");
 
 	}
 
